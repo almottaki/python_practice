3e3f104687abb199bcf45f4d285575ed726000a1
almottaki <al.mottaki000@gmail.com>
2023-08-12 14:57:03 +0600

    Change apeared

---
+++ Jsontest.py
@@ -0,0 +1,93 @@
+# json means (JavaSript Objest Notation)
+# json format :
+#            1.json array object (Array need for Multiple data)
+#            2.formatted string
+#            3.key value pair
+#
+# Why Json?
+#    For Rest API
+
+
+
+
+
+
+# import json
+#
+# x =  '{"name":"MOTTAKI", "age":"21", "home":"Rangpur"}'
+#
+# y = json.loads(x)
+#
+# print(y["home"])
+
+
+
+# import json
+#
+# data = {
+#     "MOTTAKI": [
+#         {
+#             "Age": "21",
+#             "Date of Birth": "16-09-2002",
+#             "Education": [
+#             "JSC",
+#             "SSC",
+#             "DIPLOMA"
+#             ]
+#         }
+# ],
+#     "MARJIA": [
+#         {
+#             "Age": "15",
+#             "Date of Birth": "13-12-2008",
+#             "Education": [
+#             "PSC",
+#             ]
+#         }
+#
+# ]
+# }
+#
+# y = json.dumps(data)
+#
+# print(y)
+
+
+# import json
+#
+# print(json.dumps({"name": "John", "age": 30}))
+# print(json.dumps(["apple", "banana"]))
+# print(json.dumps(("apple", "banana")))
+# print(json.dumps("hello"))
+# print(json.dumps(42))
+# print(json.dumps(31.76))
+# print(json.dumps(True))
+# print(json.dumps(False))
+# print(json.dumps(None))
+
+# import json
+#
+# x = {
+#   "name": "John",
+#   "age": 30,
+#   "married": True,
+#   "divorced": False,
+#   "children": ("Ann","Billy"),
+#   "pets": None,
+#   "cars": [
+#     {"model": "BMW 230", "mpg": 27.5},
+#     {"model": "Ford Edge", "mpg": 24.1}
+#   ]
+# }
+
+# print(json.dumps(x, indent=4))
+# print(json.dumps(x, indent=4, separators=(". ", " = ")))
+# print(json.dumps(x, indent=4, sort_keys=True))
+
+
+import json
+file=open("/home/mottaki/Documents/sample.json", "r")
+x=file.read()
+finaldata=json.loads(x)
+
+print(finaldata)
